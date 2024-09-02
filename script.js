dbbe010330df2c50a72cb811b4f18bf10d447e41
luanzura <luanmaximus@gmail.com>
2024-09-02 18:42:35 -0300

    add

--- script.js
+++ script.js
@@ -1,21 +1,10 @@
-// Valores das parcelas para cartão e Nubank
+// Valores das parcelas para cartão
 const cartao = [
   4.39, 6.48, 7.54, 8.59, 9.65, 10.7, 14.41, 15.5, 16.62, 17.73, 18.86, 19.81,
 ];
-const nubank = [
-  2.77, 5.81, 6.98, 8.17, 9.36, 10.58, 11.8, 13.01, 14.25, 15.5, 16.76, 18.03,
-];
 
-function calcularParcelas(valor, parcelas, tipo) {
-  let juros;
-  if (tipo === "cartao") {
-    juros = cartao[parcelas - 1] / 100;
-  } else if (tipo === "nubank") {
-    juros = nubank[parcelas - 1] / 100;
-  } else {
-    throw new Error("Tipo inválido");
-  }
-
+function calcularParcelas(valor, parcelas) {
+  const juros = cartao[parcelas - 1] / 100;
   const valorParcela = (valor * (1 + juros)) / parcelas;
   const valorTotal = valorParcela * parcelas;
 
@@ -37,18 +26,10 @@ function calcular() {
   // Calcula resultados para Cartão
   let resultadoCartaoHTML = "";
   for (let i = 1; i <= cartao.length; i++) {
-    const resultado = calcularParcelas(valorTotalPix, i, "cartao");
+    const resultado = calcularParcelas(valorTotalPix, i);
     resultadoCartaoHTML += `<tr><td>${i}x</td><td>R$${resultado.valorParcela}</td><td>R$${resultado.valorTotal}</td></tr>`;
   }
   document.getElementById("tbodyCartao").innerHTML = resultadoCartaoHTML;
-
-  // Calcula resultados para Nubank
-  let resultadoNubankHTML = "";
-  for (let i = 1; i <= nubank.length; i++) {
-    const resultado = calcularParcelas(valorTotalPix, i, "nubank");
-    resultadoNubankHTML += `<tr><td>${i}x</td><td>R$${resultado.valorParcela}</td><td>R$${resultado.valorTotal}</td></tr>`;
-  }
-  document.getElementById("tbodyNubank").innerHTML = resultadoNubankHTML;
 }
 
 // Calcula automaticamente ao carregar a página
@@ -59,7 +40,7 @@ function capturarImagem() {
   const resultadoDiv = document.createElement("div");
 
   // Definindo estilo para a captura de imagem
-  resultadoDiv.style.width = "50%"; // Ajusta a largura para 50%
+  resultadoDiv.style.width = "30%"; // Ajusta a largura para 50%
   resultadoDiv.style.margin = "0 auto"; // Centraliza a div na página
   resultadoDiv.style.display = "flex"; // Alinha as tabelas lado a lado
   resultadoDiv.style.flexDirection = "column"; // Coloca os elementos em coluna
@@ -76,20 +57,12 @@ function capturarImagem() {
         document.getElementById("valor").value
       ).toFixed(2)}</strong></h1>
     </div>
-    <div style="display: flex; justify-content: space-between">
-      <div style="flex: 1; margin-right: 10px;">
+    <div style="display: flex; justify-content: center">
+      <div style="flex: 1;">
         <div class="card shadow-sm mb-4">
           <div class="card-header bg-primary text-white">Parcelamento no Cartão</div>
           <div class="card-body">
             ${document.getElementById("tabelaCartao").outerHTML}
-          </div>
-        </div>
-      </div>
-      <div style="flex: 1; margin-left: 10px;">
-        <div class="card shadow-sm mb-4">
-          <div class="card-header bg-success text-white">Parcelamento no Cartão Nubank</div>
-          <div class="card-body">
-            ${document.getElementById("tabelaNubank").outerHTML}
           </div>
         </div>
       </div>
